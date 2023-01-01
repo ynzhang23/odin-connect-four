c7c423186ac17087a476202f8cd4160f96b97f82
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-01-01 21:57:32 +0800

    Comment pseudocode

---
+++ lib/main.rb
@@ -0,0 +1,16 @@
+# Create player 1
+  # Ask for player 1's name
+  # Ask for player 1's choice of ascii logo
+# Create player 2
+  # Ask for player 2's name
+  # Ask for player 2's choice of ascii logo
+
+# Loop until Winning condition is reached
+  # Update board with player 1 move
+    # Ask for player 1's move
+    # Print the board
+  # Update board with player 2 move
+    # Ask for player 2's move
+    # Print the board
+
+# Output winner
