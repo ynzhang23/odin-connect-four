d8f96520233bb88b28cc9511b56339cf54e2d07e
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-01-02 20:50:40 +0800

    Update symbol according to verified player input

--- lib/player.rb
+++ lib/player.rb
@@ -2,15 +2,41 @@
 
 # Handles player creation with player name and piece
 class Player
-  attr_reader :name, :piece
+  attr_reader :name, :symbol
 
   def initialize
-    @name = gets_name
-    @piece = nil
+    @name = nil
+    @symbol = nil
   end
 
   def gets_name
-    puts 'Please enter your name: '
+    name_question
     @name = gets.chomp
+  end
+
+  def gets_symbol
+    symbol_question
+    choice = gets.chomp until valid_symbol?(choice)
+    choice
+  end
+
+  def update_symbol
+    @symbol = '☻' if gets_symbol == '1'
+    @symbol = '☺' if gets_symbol == '2'
+  end
+
+  def valid_symbol?(choice)
+    return true if choice == '1' || choice == '2'
+    false
+  end
+
+  def symbol_question
+    puts 'Please choose your symbol:
+    1: ☻
+    2: ☺'
+  end
+
+  def name_question
+    puts 'Please enter your name: '
   end
 end
