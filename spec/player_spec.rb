54b999fa18e64a88b8ce0625cc31db428adbdb7c
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-01-02 18:32:55 +0800

    Create test for updating player symbol

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,12 +8,36 @@ describe Player do
   describe '#gets_name' do
     before do
       allow(player).to receive(:gets).and_return('James')
-      allow(player).to receive(:puts)
+      allow(player).to receive(:name_question)
     end
 
     it 'Gets the player name' do
       player.gets_name
       expect(player.name).to eql('James')
+    end
+  end
+
+  describe '#update_symbol' do
+    context 'When the player chooses option 1' do
+      before do 
+        allow(player).to receive(:gets_symbol).and_return('1')
+      end
+
+      it 'Modifies the player symbol to ☻' do
+        player.update_symbol
+        expect(player.symbol).to eql('☻')
+      end
+    end
+
+    context 'When the player chooses option 2' do
+      before do 
+        allow(player).to receive(:gets_symbol).and_return('2')
+      end
+
+      it 'Modifies the player symbol to ☺' do
+        player.update_symbol
+        expect(player.symbol).to eql('☺')
+      end
     end
   end
 end
