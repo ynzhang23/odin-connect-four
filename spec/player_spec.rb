eda411109f344904154d26fcd2cd35db0a5ef6f4
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-01-02 21:25:31 +0800

    Create test for getting player choice for symbol

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -66,4 +66,60 @@ describe Player do
       end
     end
   end
+
+  describe '#gets_symbol' do
+    context 'Input fails first, succeed on second' do
+      before do
+        allow(player).to receive(:symbol_question)
+        allow(player).to receive(:puts)
+        allow(player).to receive(:gets).and_return('3', '1')
+      end
+
+      it 'Displays error message once' do
+        expect(player).to receive(:puts).with('Input error!').once
+        player.gets_symbol
+      end
+
+      it 'Returns the choice' do
+        result = player.gets_symbol
+        expect(result).to eql('1')
+      end
+    end
+
+    context 'Input fails twice, succeed on third' do
+      before do
+        allow(player).to receive(:symbol_question)
+        allow(player).to receive(:puts)
+        allow(player).to receive(:gets).and_return('3', '', '2')
+      end
+
+      it 'Displays error message twice' do
+        expect(player).to receive(:puts).with('Input error!').twice
+        player.gets_symbol
+      end
+
+      it 'Returns the choice' do
+        result = player.gets_symbol
+        expect(result).to eql('2')
+      end
+    end
+
+    context 'Input succeed on first' do
+      before do
+        allow(player).to receive(:symbol_question)
+        allow(player).to receive(:puts)
+        allow(player).to receive(:gets).and_return('1')
+      end
+
+      it 'Does not display error message' do
+        expect(player).to_not receive(:puts).with('Input error!')
+        player.gets_symbol
+      end
+
+      it 'Returns the choice' do
+        result = player.gets_symbol
+        expect(result).to eql('1')
+      end
+    end
+  end
 end
