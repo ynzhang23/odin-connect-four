1a0e246e3f05cce38c6747e954f202ccf96f5945
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-01-03 17:37:56 +0800

    Update the board positions after getting player choice

--- lib/board.rb
+++ lib/board.rb
@@ -12,24 +12,33 @@ class Board
 
   def create_board
     {
-      '1': [],
-      '2': [],
-      '3': [],
-      '4': [],
-      '5': [],
-      '6': [],
-      '7': []
+      '1' => [],
+      '2' => [],
+      '3' => [],
+      '4' => [],
+      '5' => [],
+      '6' => [],
+      '7' => []
     }
   end
 
   def gets_move
-    puts 'Please select input column'
-    column = gets.chomp until valid_column?
+    puts 'Please select a column'
+    column = gets.chomp
+    until valid_column?(column)
+      puts 'Invalid column'
+      column = gets.chomp
+    end
+    while full_column?(column)
+      puts 'Column full'
+      column = gets.chomp
+    end
+    column
   end
 
   def valid_column?(column)
     input = column.to_i
-    return true if input > 0 && input < 8
+    return true if input.positive? && input < 8
 
     false
   end
@@ -38,5 +47,10 @@ class Board
     return true if @columns[column].length >= 6
 
     false
+  end
+
+  def update_board(player)
+    column_number = gets_move
+    @columns[column_number].push(player.symbol)
   end
 end
