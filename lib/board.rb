d970019131b589e9ad23fdbe7453e21708494d55
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-01-03 17:54:28 +0800

    Allow board to print itself

--- lib/board.rb
+++ lib/board.rb
@@ -22,6 +22,26 @@ class Board
     }
   end
 
+  def print_board
+    one = @columns['1']
+    two = @columns['2']
+    three = @columns['3']
+    four = @columns['4']
+    five = @columns['5']
+    six = @columns['6']
+    seven = @columns['7']
+
+    puts "
+    6 #{one[5]} #{two[5]} #{three[5]} #{four[5]} #{five[5]} #{six[5]} #{seven[5]}
+    5 #{one[4]} #{two[4]} #{three[4]} #{four[4]} #{five[4]} #{six[4]} #{seven[4]}
+    4 #{one[3]} #{two[3]} #{three[3]} #{four[3]} #{five[3]} #{six[3]} #{seven[3]}
+    3 #{one[2]} #{two[2]} #{three[2]} #{four[2]} #{five[2]} #{six[2]} #{seven[2]}
+    2 #{one[1]} #{two[1]} #{three[1]} #{four[1]} #{five[1]} #{six[1]} #{seven[1]}
+    1 #{one[0]} #{two[0]} #{three[0]} #{four[0]} #{five[0]} #{six[0]} #{seven[0]}
+      1 2 3 4 5 6 7
+    "
+  end
+
   def update_board(player)
     column_number = gets_move
     @columns[column_number].push(player.symbol)
