8bf107f14388420e7787298d0af0728c4a7eb07d
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-01-04 22:44:48 +0800

    Update board design

--- lib/board.rb
+++ lib/board.rb
@@ -33,13 +33,13 @@ class Board
     seven = @columns[7]
 
     puts "
-      #{one[5]} #{two[5]} #{three[5]} #{four[5]} #{five[5]} #{six[5]} #{seven[5]}
-      #{one[4]} #{two[4]} #{three[4]} #{four[4]} #{five[4]} #{six[4]} #{seven[4]}
-      #{one[3]} #{two[3]} #{three[3]} #{four[3]} #{five[3]} #{six[3]} #{seven[3]}
-      #{one[2]} #{two[2]} #{three[2]} #{four[2]} #{five[2]} #{six[2]} #{seven[2]}
-      #{one[1]} #{two[1]} #{three[1]} #{four[1]} #{five[1]} #{six[1]} #{seven[1]}
-      #{one[0]} #{two[0]} #{three[0]} #{four[0]} #{five[0]} #{six[0]} #{seven[0]}
-      1 2 3 4 5 6 7
+      6 #{one[5]} #{two[5]} #{three[5]} #{four[5]} #{five[5]} #{six[5]} #{seven[5]}
+      5 #{one[4]} #{two[4]} #{three[4]} #{four[4]} #{five[4]} #{six[4]} #{seven[4]}
+      4 #{one[3]} #{two[3]} #{three[3]} #{four[3]} #{five[3]} #{six[3]} #{seven[3]}
+      3 #{one[2]} #{two[2]} #{three[2]} #{four[2]} #{five[2]} #{six[2]} #{seven[2]}
+      2 #{one[1]} #{two[1]} #{three[1]} #{four[1]} #{five[1]} #{six[1]} #{seven[1]}
+      1 #{one[0]} #{two[0]} #{three[0]} #{four[0]} #{five[0]} #{six[0]} #{seven[0]}
+        1 2 3 4 5 6 7
     "
   end
 
